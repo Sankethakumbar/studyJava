942a0795b1826663c090ce8db690cf8afad26207
Sanketha_Kumbar <sankethkumbar@gmail.com>
2026-02-10 08:09:25 +0530

    LeapYear

--- challenges/leapYear.java
+++ challenges/leapYear.java
@@ -5,7 +5,7 @@ public class leapYear {
         System.out.println("Enter the year");
         int year =input.nextInt();
         input.close();
-        if(year%400==0 || year%100!=0 || year%4==0){
+        if(year%400==0 || (year%100!=0 && year%4==0)){
             System.out.println("IT IS A LEAP YEAR");
         }else{
             System.out.println("It is not a leap year");
